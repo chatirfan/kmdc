f088cd82c6af19e99184d53c19c89a05e15e741a
Shoaib Ahmed Khan <shoaibkhan105@live.com>
2013-07-01 03:53:16 +0500

    Ion Auth working
    Ion Auth configured and in working condition

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '453kjhg54j6jregh54kj45k6';
 
 /*
 |--------------------------------------------------------------------------
