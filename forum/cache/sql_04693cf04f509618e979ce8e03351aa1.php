ba69ee1520e06e7e6de13db985f71a93c8bc38cf
khanjeee <shoaibkhan105@live.com>
2013-07-18 22:11:43 +0500

    ...

--- forum/cache/sql_04693cf04f509618e979ce8e03351aa1.php
+++ forum/cache/sql_04693cf04f509618e979ce8e03351aa1.php
@@ -1,21 +1,5 @@
 <?php exit; ?>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-1374021570
-=======
-1374005516
->>>>>>> 0767f65ea01e65123240d72c0d588ca4079f7da0
-=======
-1374005516
->>>>>>> 0767f65ea01e65123240d72c0d588ca4079f7da0
-=======
-1374005516
->>>>>>> 0767f65ea01e65123240d72c0d588ca4079f7da0
-=======
-1374005516
->>>>>>> 0767f65ea01e65123240d72c0d588ca4079f7da0
+1374170052
 SELECT ban_ip, ban_userid, ban_email, ban_exclude, ban_give_reason, ban_end FROM forum_banlist WHERE ban_email = '' AND (ban_userid = 1 OR ban_ip <> '')
 6
 a:0:{}
